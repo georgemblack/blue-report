17237fba3dd365cb25704d920ffdec1b5280dc48
George Black <contact@george.black>
2024-12-31 16:10:00 -0600

    Visual cleanup and error logging

--- pkg/app/util/time.go
+++ pkg/app/util/time.go
@@ -1,10 +1,14 @@
 package util
 
-import "time"
+import (
+	"log/slog"
+	"time"
+)
 
 func ToEastern(t time.Time) time.Time {
 	location, err := time.LoadLocation("America/New_York")
 	if err != nil {
+		slog.Error(WrapErr("failed to load time location when converting time to eastern", err).Error())
 		return t
 	}
 	return t.In(location)
