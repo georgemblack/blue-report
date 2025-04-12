90948631e7b6a960f5c9ec1fb7dbb6bcada10fb5
George Black <contact@george.black>
2025-04-11 19:23:20 -0500

    Use Cloudflare adapter for Astro to migrate from Pages to Workers

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://theblue.report",
   integrations: [sitemap()],
+  adapter: cloudflare(),
 });
