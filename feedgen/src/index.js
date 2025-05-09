03a66143383d7be755722e8ee1b0401c4a0aaad9
George Black <contact@george.black>
2025-05-09 06:49:59 -0500

    Add scaffolding to feedgen worker

--- feedgen/src/index.js
+++ feedgen/src/index.js
@@ -1,15 +1,45 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    if (url.pathname === "/.well-known/did.json") return handleDidRequest();
+    if (url.pathname === "/xrpc/app.bsky.feed.getFeedSkeleton")
+      return handleFeedRequest();
+    return new Response("Not Found", {
+      status: 404,
+    });
+  },
 };
+
+async function handleDidRequest() {
+  return new Response(
+    JSON.stringify({
+      "@context": ["https://www.w3.org/ns/did/v1"],
+      id: "did:web:feedgen.theblue.report",
+      service: [
+        {
+          id: "#bsky_fg",
+          type: "BskyFeedGenerator",
+          serviceEndpoint: "https://feedgen.theblue.report",
+        },
+      ],
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+}
+
+async function handleFeedRequest() {
+  return new Response(
+    JSON.stringify({
+      data: "ahh",
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+}
